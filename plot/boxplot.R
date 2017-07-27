3d3681d54a97ff6a38db180e4a90af24e360bb5c
matthew <wb2847@163.com>
2017-07-27 14:17:38 +0800

    box plot

---
+++ plot/boxplot.R
@@ -0,0 +1,4 @@
+mtcars$cyl.f<-factor(mtcars$cyl,levels = c(4,6,8),labels = c("4","6","8"))
+mtcars$am.f<-factor(mtcars$am,levels = c(0,1),labels=c("auto","standard"))
+boxplot(mpg~am.f*cyl.f,data=mtcars,varwidth=TRUE,col=c("gold","green"),main="boxlot",xlab="auto type")
+
